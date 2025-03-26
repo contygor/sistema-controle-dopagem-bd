f892e2ecc47497e805aca054f302ffe0d37bd939
Ygor Araujo <158191271+contygor@users.noreply.github.com>
2025-03-26 09:24:30 -0300

    Update schema_controle_dopagem.sql

--- schema_controle_dopagem.sql
+++ schema_controle_dopagem.sql
@@ -1,166 +1,170 @@
-create database controle_dopagem;
-use controle_dopagem;
+CREATE DATABASE controle_dopagem;
+USE controle_dopagem;
 
-create table documento (
-    id int auto_increment primary key,
-    tipo_documento varchar(50),
-    numero_documento varchar(50)
+CREATE TABLE documento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_documento VARCHAR(50),
+    numero_documento VARCHAR(50)
 );
 
-create table medico (
-    id int auto_increment primary key,
-    nome varchar(70),
-    sobrenome varchar(100)
+CREATE TABLE medico (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(70),
+    sobrenome VARCHAR(100)
 );
 
-create table tecnico (
-    id int auto_increment primary key,
-    nome varchar(70),
-    sobrenome varchar(100)
+CREATE TABLE tecnico (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(70),
+    sobrenome VARCHAR(100)
 );
 
-create table atleta (
-    id int auto_increment primary key,
-    id_documento int not null,
-    foreign key (id_documento) references documento(id),
-    id_tecnico int not null,
-    foreign key (id_tecnico) references tecnico(id),
-    id_medico int not null,
-    foreign key (id_medico) references medico(id),
-    nome varchar(70),
-    sobrenome varchar(100),
-    sexo char(1),
-    nacionalidade varchar(50),
-    email varchar(70),
-    nascimento date,
-    telefone varchar(17),
-    assinatura blob
+CREATE TABLE endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rua VARCHAR(50),
+    numero VARCHAR(5),
+    cep VARCHAR(15),
+    estado VARCHAR(50),
+    pais VARCHAR(50)
 );
 
-create table endereco (
-    id int auto_increment primary key,
-    id_atleta int not null,
-    foreign key (id_atleta) references atleta(id),
-    rua varchar(50),
-    numero varchar(5),
-    cep varchar(15),
-    estado varchar(50),
-    pais varchar(50)
+CREATE TABLE atleta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_documento INT NOT NULL,
+    FOREIGN KEY (id_documento) REFERENCES documento(id),
+    id_tecnico INT NOT NULL,
+    FOREIGN KEY (id_tecnico) REFERENCES tecnico(id),
+    id_medico INT NOT NULL,
+    FOREIGN KEY (id_medico) REFERENCES medico(id),
+    nome VARCHAR(70),
+    sobrenome VARCHAR(100),
+    sexo CHAR(1),
+    nacionalidade VARCHAR(50),
+	id_endereco INT NOT NULL,
+    FOREIGN KEY (id_endereco) REFERENCES endereco(id),
+    email VARCHAR(70),
+    nascimento DATE,
+    telefone VARCHAR(17),
+    assinatura BLOB
 );
 
-create table escolta (
-    id int auto_increment primary key,
-    data_escolta date,
-    hora_escolta time,
-    nome_escolta varchar(170),
-    assinatura_escolta blob
+CREATE TABLE escolta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_escolta DATE,
+    hora_escolta TIME,
+    nome_escolta VARCHAR(170),
+    assinatura_escolta BLOB
 );
 
-create table autoridades (
-    id int auto_increment primary key,
-    autoridade_teste varchar(50),
-    autoridade_coleta varchar(50),
-    autoridade_gestao varchar(50),
-    autoridade_controle varchar(50)
+CREATE TABLE autoridades (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    autoridade_teste VARCHAR(50),
+    autoridade_coleta VARCHAR(50),
+    autoridade_gestao VARCHAR(50),
+    autoridade_controle VARCHAR(50)
 );
 
-create table equipamento (
-    id int auto_increment primary key,
-    marca_fabricante varchar(50),
-    modelo varchar(50)
+CREATE TABLE equipamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marca_fabricante VARCHAR(50),
+    modelo VARCHAR(50)
 );
 
-create table evento (
-    id int auto_increment primary key,
-    competicao varchar(80),
-    esporte varchar(50),
-    funcao_atleta varchar(70)
+CREATE TABLE evento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    competicao VARCHAR(80),
+    esporte VARCHAR(50),
+    funcao_atleta VARCHAR(70)
 );
 
-create table representante_do_atleta (
-    id int auto_increment primary key,
-    nome varchar(70),
-    sobrenome varchar(100),
-    funcao varchar(70),
-    assinatura blob
+CREATE TABLE representante_do_atleta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(70),
+    sobrenome VARCHAR(100),
+    funcao VARCHAR(70),
+    assinatura BLOB
 );
 
-create table confirmacao (
-    id int auto_increment primary key,
-    data_confirmacao date,
-    hora_finalizacao_exames time,
-    id_representante_do_atleta int,
-    foreign key (id_representante_do_atleta) references representante_do_atleta(id)
+CREATE TABLE confirmacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_confirmacao DATE,
+	hora_finalizacao_exames TIME, 
+	id_representante_do_atleta INT, 
+	FOREIGN KEY (id_representante_do_atleta )
+
+CREATE TABLE oficial_de_controle (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150),
+    assinatura BLOB
 );
 
-create table oficial_de_controle (
-    id int auto_increment primary key,
-    nome varchar(150),
-    assinatura blob
+CREATE TABLE testemunha (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(70),
+    sobrenome VARCHAR(100)
 );
 
-create table testemunha (
-    id int auto_increment primary key,
-    nome varchar(70),
-    sobrenome varchar(100)
+CREATE TABLE amostra_parcial (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    volume_amostra VARCHAR(7),
+    dia_hora_selagem DATETIME
 );
 
-create table amostra_parcial (
-    id int auto_increment primary key,
-    volume_amostra varchar(7),
-    dia_hora_selagem datetime
+CREATE TABLE tipo_amostra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_tipo VARCHAR(100),
+    data_hora_selagem DATETIME,
+    densidade_amostra VARCHAR(7),
+    volume_amostra VARCHAR(7)
 );
 
-create table tipo_amostra (
-    id int auto_increment primary key,
-    nome_tipo varchar(100),
-    data_hora_selagem datetime,
-    densidade_amostra varchar(7),
-    volume_amostra varchar(7)
+CREATE TABLE amostra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_atleta INT NOT NULL,
+    FOREIGN KEY (id_atleta) REFERENCES atleta(id),
+    id_escolta INT NOT NULL,
+    FOREIGN KEY (id_escolta) REFERENCES escolta(id),
+    id_autoridades INT NOT NULL,
+    FOREIGN KEY (id_autoridades) REFERENCES autoridades(id),
+    id_equipamento INT NOT NULL,
+    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id),
+    id_oficial_controle INT NOT NULL,
+    FOREIGN KEY (id_oficial_controle) REFERENCES oficial_de_controle(id),
+    id_evento INT NOT NULL,
+    FOREIGN KEY (id_evento) REFERENCES evento(id),
+    id_confirmacao INT NOT NULL,
+    FOREIGN KEY (id_confirmacao) REFERENCES confirmacao(id),
+    data_coleta DATE,
+    hora_chegada_atleta TIME,
+    codigo_relatorio_suplementar VARCHAR(15)
 );
 
-create table amostra (
-    id int auto_increment primary key,
-    id_atleta int not null,
-    foreign key (id_atleta) references atleta(id),
-    id_escolta int not null,
-    foreign key (id_escolta) references escolta(id),
-    id_autoridades int not null,
-    foreign key (id_autoridades) references autoridades(id),
-    id_equipamento int not null,
-    foreign key (id_equipamento) references equipamento(id),
-    id_oficial_controle int not null,
-    foreign key (id_oficial_controle) references oficial_de_controle(id),
-    id_evento int not null,
-    foreign key (id_evento) references evento(id),
-    id_confirmacao int not null,
-    foreign key (id_confirmacao) references confirmacao(id),
-    data_coleta date,
-    hora_chegada_atleta time,
-    codigo_relatorio_suplementar varchar(15)
+CREATE TABLE testemunhas_dos_tipos_amostra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_tipo_amostra INT NOT NULL,
+    FOREIGN KEY (id_tipo_amostra) REFERENCES tipo_amostra(id),
+    id_testemunha INT NOT NULL,
+    FOREIGN KEY (id_testemunha) REFERENCES testemunha(id)
 );
 
-create table testemunhas_dos_tipos_amostra (
-    id int auto_increment primary key,
-    id_tipo_amostra int not null,
-    foreign key (id_tipo_amostra) references tipo_amostra(id),
-    id_testemunha int not null,
-    foreign key (id_testemunha) references testemunha(id)
+CREATE TABLE tipos_amostra_da_amostra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_amostra INT NOT NULL,
+    FOREIGN KEY (id_amostra) REFERENCES amostra(id),
+    id_tipo_amostra INT NOT NULL,
+    FOREIGN KEY (id_tipo_amostra) REFERENCES tipo_amostra(id)
 );
 
-create table tipos_amostra_da_amostra (
-    id int auto_increment primary key,
-    id_amostra int not null,
-    foreign key (id_amostra) references amostra(id),
-    id_tipo_amostra int not null,
-    foreign key (id_tipo_amostra) references tipo_amostra(id)
+CREATE TABLE amostras_parciais_da_amostra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_amostra INT NOT NULL,
+    FOREIGN KEY (id_amostra) REFERENCES amostra(id),
+    id_amostra_parcial INT NOT NULL,
+    FOREIGN KEY (id_amostra_parcial) REFERENCES amostra_parcial(id)
 );
 
-create table amostras_parciais_da_amostra (
-    id int auto_increment primary key,
-    id_amostra int not null,
-    foreign key (id_amostra) references amostra(id),
-    id_amostra_parcial int not null,
-    foreign key (id_amostra_parcial) references amostra_parcial(id)
-);
+ALTER TABLE endereco
+ADD COLUMN cidade VARCHAR(50) AFTER rua;
 
+ALTER TABLE medico
+ADD COLUMN crm VARCHAR(20);
